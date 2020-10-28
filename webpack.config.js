f0d3b033743e2bac7f808580e601f26e0f471ccd
Nikita Bukin <locus2@mail.ru>
2020-10-28 14:31:00 +0300

    i&cnf clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ const delay = (timeout = 1000) => {
   new Promise(resolve => setTimeout(resolve, timeout));
 };
 
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+// Добавляем модуль path для отработки аутпута
+const path = require('path');
 /*
 * Тпиы конфигов вебпака:
 * Object
@@ -16,11 +19,17 @@ module.exports = () => {
   return {    
     mode: 'none',
     devtool: false,
+    output: {
+      path: path.resolve(process.cwd(), 'dist')
+    },
     plugins: [
       // Каждый плагин - это конструктор
       new HtmlWebpackPlugin({
         template: './static/template.html',
         title: 'Изучаем webpack!'
+      }),
+      new CleanWebpackPlugin({
+        dry: true // Симуляия удаления файлов с выводом в консоль  
       }),
     ]
   };
